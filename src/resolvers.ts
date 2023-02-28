a883ae3732177314039b1ae03bc36a173602570a
hangleang <hangleang127@gmail.com>
2023-02-28 17:16:39 +0700

    feat(resolver): inverse lookup resolver from related sources

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,143 +1,158 @@
 import { Resolvers } from '../.graphclient'
 
 const resolvers: Resolvers = {
-  NFToken: {
-    myToken: {
+  Account: {
+    asUser: {
       selectionSet: /* GraphQL */ `{ id }`,
-      resolve: async (root, _args, context, info) => await context.WTFMarketplace.Query.token({
+      resolve: async (root, _args, context, info) => await context.WTFMarketplace.Query.user({
         root, context, info, args: { id: root.id }
       })
-    }
+    },
   },
-  NFTCollection: {
-    myCollection: {
+  User: {
+    asAccount: {
+      selectionSet: /* GraphQL */ `{ id }`,
+      resolve: async (root, _args, context, info) => await context.NFTs.Query.account({
+        root, context, info, args: { id: root.id }
+      })
+    },
+  },
+  ERC721Contract: {
+    asMyCollection: {
       selectionSet: /* GraphQL */ `{ id }`,
       resolve: async (root, _args, context, info) => await context.WTFMarketplace.Query.collection({
         root, context, info, args: { id: root.id }
       })
+    },
+    asOpenseaCollection: {
+      selectionSet: /* GraphQL */ `{ id }`,
+      resolve: async (root, _args, context, info) => await context.OpenSeaSeaport.Query.os_collection({
+        root, context, info, args: { id: root.id }
+      })
+    }
+  },
+  ERC1155Contract: {
+    asMyCollection: {
+      selectionSet: /* GraphQL */ `{ id }`,
+      resolve: async (root, _args, context, info) => await context.WTFMarketplace.Query.collection({
+        root, context, info, args: { id: root.id }
+      })
+    },
+    asOpenseaCollection: {
+      selectionSet: /* GraphQL */ `{ id }`,
+      resolve: async (root, _args, context, info) => await context.OpenSeaSeaport.Query.os_collection({
+        root, context, info, args: { id: root.id }
+      })
+    }
+  },
+  ERC721Token: {
+    asMyToken: {
+      selectionSet: /* GraphQL */ `{ id }`,
+      resolve: async (root, _args, context, info) => await context.WTFMarketplace.Query.token({
+        root, context, info, args: { id: root.id }
+      })
+    },
+    asOpenseaListings: {
+      selectionSet: /* GraphQL */ `{ contract { id } identifier }`,
+      resolve: async (root, _args, context, info) => await context.OpenSeaAPI.Query.openseaListings({
+        root, context, info, args: { asset_contract_address: root.contract.id, token_ids: root.identifier }
+      })
+    },
+    asOpenseaOffers: {
+      selectionSet: /* GraphQL */ `{ contract { id } identifier }`,
+      resolve: async (root, _args, context, info) => await context.OpenSeaAPI.Query.openseaOffers({
+        root, context, info, args: { asset_contract_address: root.contract.id, token_ids: root.identifier }
+      })
+    }
+  },
+  ERC1155Token: {
+    asMyToken: {
+      selectionSet: /* GraphQL */ `{ id }`,
+      resolve: async (root, _args, context, info) => await context.WTFMarketplace.Query.token({
+        root, context, info, args: { id: root.id }
+      })
+    },
+    asOpenseaListings: {
+      selectionSet: /* GraphQL */ `{ contract { id } identifier }`,
+      resolve: async (root, _args, context, info) => await context.OpenSeaAPI.Query.openseaListings({
+        root, context, info, args: { asset_contract_address: root.contract.id, token_ids: root.identifier }
+      })
+    },
+    asOpenseaOffers: {
+      selectionSet: /* GraphQL */ `{ contract { id } identifier }`,
+      resolve: async (root, _args, context, info) => await context.OpenSeaAPI.Query.openseaOffers({
+        root, context, info, args: { asset_contract_address: root.contract.id, token_ids: root.identifier }
+      })
     }
   },
   Collection: {
-    nftCollection: {
+    asERC721: {
       selectionSet: /* GraphQL */ `{ id, collectionType }`,
       resolve: async (root, _args, context, info) => {
-        if (root.collectionType == "SingleEdition") {
-          return context.NFTs.Query.erc721Contract({
-            root, context, info, args: { id: root.id }
-          })
-        } else if (root.collectionType == "MultiEdition") {
-          return context.NFTs.Query.erc1155Contract({
-            root, context, info, args: { id: root.id }
-          })
-        } else {
-          return null;
-        }
+        if (root.collectionType != "SingleEdition") return null;
+        return context.NFTs.Query.erc721Contract({
+          root, context, info, args: { id: root.id }
+        })
+      }
+    },
+    asERC1155: {
+      selectionSet: /* GraphQL */ `{ id, collectionType }`,
+      resolve: async (root, _args, context, info) => {
+        if (root.collectionType != "MultiEdition") return null;
+        return context.NFTs.Query.erc1155Contract({
+          root, context, info, args: { id: root.id }
+        })
       }
     }
   },
   Token: {
-    nftToken: {
+    asERC721Token: {
       selectionSet: /* GraphQL */ `{ id, collection { collectionType } }`,
       resolve: async (root, _args, context, info) => {
-        if (root.collection.collectionType == "SingleEdition") {
-          return context.NFTs.Query.erc721Token({
-            root, context, info, args: { id: root.id }
-          })
-        } else if (root.collection.collectionType == "MultiEdition") {
-          return context.NFTs.Query.erc1155Token({
-            root, context, info, args: { id: root.id }
-          })
-        } else {
-          return null;
-        }
+        if (root.collection.collectionType != "SingleEdition") return null;
+        return context.NFTs.Query.erc721Token({
+          root, context, info, args: { id: root.id }
+        })
+      }
+    },
+    asERC1155Token: {
+      selectionSet: /* GraphQL */ `{ id, collection { collectionType } }`,
+      resolve: async (root, _args, context, info) => {
+        if (root.collection.collectionType != "MultiEdition") return null;
+        return context.NFTs.Query.erc1155Token({
+          root, context, info, args: { id: root.id }
+        })
       }
     }
-  }
-  // OpenSeaAllListingsResponse: {
-  //   myCollection: {
-  //     selectionSet: `{
-  //       listings
-  //     }`,
-  //     resolve(root, _args, context, info) {
-  //       const id = root.listings?.at(0)?.protocol_data?.parameters?.offer?.at(0)?.token?.toLowerCase();
-        
-  //       if (!id) return null;
-
-  //       return context.WTFMarketplace.Query.collection({
-  //         root,
-  //         context,
-  //         info,
-  //         args: {
-  //           id
-  //         }
-  //       })
-  //     }
-  //   }
-  // },
-  // Collection: {
-  //   openseaAllListings: {
-  //     selectionSet: /* GraphQL */ `
-  //       {
-  //         id
-  //       }
-  //     `,
-  //     resolve(root, _args, context, info) {
-  //       return context.OpenSeaAPI.Query.openseaAllListings({
-  //         root, context, info, args: { slug: root.id }
-  //       })
-  //     }
+  },
+  OS_Collection: {
+    openseaAllListings: {
+      selectionSet: /* GraphQL */ `{ id }`,
+      resolve: async (root, _args, context, info) => context.OpenSeaAPI.Query.openseaAllListings({
+        root, context, info, args: { slug: root.id }
+      })
+    },
+    openseaAllOffers: {
+      selectionSet: /* GraphQL */ `{ id }`,
+      resolve: async (root, _args, context, info) => context.OpenSeaAPI.Query.openseaAllOffers({
+        root, context, info, args: { slug: root.id }
+      })
+    }
+  },
+  // Query: {
+  //   marketplaces: async (root, _args, context, info) => {
+  //     return Promise.all([
+  //       context.WTFMarketplace.Query.marketplaces({root, context, info}),
+  //       context.OpenSeaSeaport.Query.marketplaces({root, context, info})
+  //     ]).then((all) => all.flat())
   //   },
-  //   openseaAllOffers: {
-  //     selectionSet: /* GraphQL */ `
-  //       {
-  //         id
-  //       }
-  //     `,
-  //     resolve(root, _args, context, info) {
-  //       return context.OpenSeaAPI.Query.openseaAllOffers({
-  //         root, context, info, args: { slug: root.id }
-  //       })
-  //     }
+  //   marketplace: async (root, args, context, info) => {
+  //     return Promise.all([
+  //       context.WTFMarketplace.Query.marketplace({root, context, info, args}),
+  //       context.OpenSeaSeaport.Query.marketplace({root, context, info, args})
+  //     ]).then((all) => all.find((e) => e != null || e != undefined) || null)
   //   }
-  // },
-  // Token: {
-  //   openseaListings: {
-  //     selectionSet: /* GraphQL */ `
-  //       {
-  //         collection { id }, tokenId
-  //       }
-  //     `,
-  //     resolve(root, _args, context, info) {
-  //       return context.OpenSeaAPI.Query.openseaListings({
-  //         root,
-  //         args: {
-  //           asset_contract_address: root.collection.id,
-  //           token_ids: root.tokenId
-  //         },
-  //         context,
-  //         info
-  //       })
-  //     } 
-  //   },
-  //   openseaOffers: {
-  //     selectionSet: /* GraphQL */ `
-  //       {
-  //         collection { id }, tokenId
-  //       }
-  //     `,
-  //     resolve(root, _args, context, info) {
-  //       return context.OpenSeaAPI.Query.openseaOffers({
-  //         root,
-  //         args: {
-  //           asset_contract_address: root.collection.id,
-  //           token_ids: root.tokenId
-  //         },
-  //         context,
-  //         info
-  //       });
-  //     }
-  //   }
-  // },
+  // }
 }
 
 export default resolvers;
